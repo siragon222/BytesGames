2d769edef5fc64936ec26d349b62f241c778b2f4
Ronald <siragon222@gmail.com>
2025-06-30 21:36:56 -0400

    mejora de divisas

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -52,19 +52,10 @@ const Header2 = () => {
           </Link>
         </div>
         <nav ref={menuRef}>
-          <div className="mobile-menu-icon" onClick={toggleMenu} style={isMobile ? { paddingLeft: '12px' } : {}}>
-            <svg
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M3 18H21V16H3V18ZM3 13H21V11H3V13ZM3 6V8H21V6H3Z"
-                fill="white"
-              />
-            </svg>
+          <div className={`mobile-menu-icon ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+            <span></span>
+            <span></span>
+            <span></span>
           </div>
           <ul className={`nav-links ${isMenuOpen ? 'active' : ''}`}>
             <li>
